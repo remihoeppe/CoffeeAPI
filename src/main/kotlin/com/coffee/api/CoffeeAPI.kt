d9e737aae1598deca9eb351bc949fc855c217399
Remi HOEPPE <remihoeppe@users.noreply.github.com>
2024-10-21 10:42:30 +0100

    Adding drafts tests

--- src/main/kotlin/com/coffee/api/CoffeeAPI.kt
+++ src/main/kotlin/com/coffee/api/CoffeeAPI.kt
@@ -2,7 +2,6 @@ package com.coffee.api
 
 
 import org.http4k.core.Method.GET
-import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.then
@@ -13,13 +12,12 @@ import org.http4k.routing.routes
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 
-
-fun coffeeAPI() = routes(
-    "/" bind GET to { request: Request ->
-        Response(OK).body("Coffee API")
-    }
-).withFilter(DebuggingFilters.PrintRequestAndResponse().then(ServerFilters.CatchAll()))
-
 fun main() {
     coffeeAPI().asServer(SunHttp(port = 9000)).start()
 }
+
+fun coffeeAPI() = routes(
+    "/" bind GET to {
+        Response(OK).body("Coffee API")
+    }
+).withFilter(DebuggingFilters.PrintRequestAndResponse().then(ServerFilters.CatchAll()))
--- src/test/kotlin/com/coffee/api/CoffeeAPITest.kt
+++ src/test/kotlin/com/coffee/api/CoffeeAPITest.kt
@@ -1,9 +1,11 @@
 package com.coffee.api
 
 import org.http4k.core.Method.GET
+import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
+import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Status.Companion.OK
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -16,6 +18,34 @@ class CoffeeAPITest {
     fun `Testing server root endpoint`() {
         api(Request(GET, "/")).expectOK()
     }
+
+    @Test
+    fun `API returns a list of roasters GET request`() {
+        val response = api(Request(GET, "/roasters")).expectOK()
+        assertEquals("some json", response.bodyString())
+    }
+
+    @Test
+    fun `API returns a specific roaster when GET requests contains a name parameter`() {
+        val response = api(Request(GET, "/roasters/{name}"))
+        assertEquals(Roaster("name", "url", "address"), response.bodyString())
+    }
+
+    @Test
+    fun `API should return 404 when invalid name parameter is passed on GET request`() {
+        val invalidRoasterName = "invalid"
+        val response = api(Request(GET, "/roasters/$invalidRoasterName"))
+        assertEquals(NOT_FOUND, response.status)
+    }
+
+    @Test
+    fun `API should return a 204 when valid data has been sent through a POST request`() {
+        val data = Roaster("name", "url", "address")
+        val response = api(Request(POST, "/roasters", ).body(data))
+    }
+
+
+
 }
 
 private fun Response.expectOK(): Response {
